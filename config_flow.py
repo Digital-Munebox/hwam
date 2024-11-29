7e98779cad301b90772d598c6c437e5a917f29bd
Digital-Munebox <pinaud.adrien@free.fr>
2024-11-29 16:14:38 +0100

    Update config_flow.py

--- config_flow.py
+++ config_flow.py
@@ -1,17 +1,19 @@
 """Config flow for HWAM integration."""
 import voluptuous as vol
 from homeassistant import config_entries
-from homeassistant.const import CONF_HOST
+from homeassistant.const import CONF_HOST, CONF_NAME, CONF_SCAN_INTERVAL
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 import logging
 
 from .api import HWAMApi
-from .const import DOMAIN
+from .const import DOMAIN, DEFAULT_NAME, DEFAULT_SCAN_INTERVAL
 
 _LOGGER = logging.getLogger(__name__)
 
 DATA_SCHEMA = vol.Schema({
     vol.Required(CONF_HOST): str,
+    vol.Required(CONF_NAME, default=DEFAULT_NAME): str,
+    vol.Required(CONF_SCAN_INTERVAL, default=DEFAULT_SCAN_INTERVAL): int,
 })
 
 class HWAMConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -31,14 +33,12 @@ class HWAMConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             api = HWAMApi(user_input[CONF_HOST], session)
 
             try:
-                # Test the connection
-                _LOGGER.debug("Testing connection to HWAM stove")
                 if await api.async_validate_connection():
                     _LOGGER.debug("Connection test successful")
                     await api.close()
                     return self.async_create_entry(
-                        title="HWAM Stove",
-                        data={CONF_HOST: user_input[CONF_HOST]}
+                        title=user_input[CONF_NAME],
+                        data=user_input
                     )
                 
                 _LOGGER.error("Could not connect to HWAM stove")
