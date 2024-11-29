e6d7fd95b5121c8db28ae6be3408f87a3c2888cf
Digital-Munebox <pinaud.adrien@free.fr>
2024-11-29 16:34:13 +0100

    Update config_flow.py

--- config_flow.py
+++ config_flow.py
@@ -2,9 +2,7 @@
 import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.const import CONF_HOST, CONF_NAME, CONF_SCAN_INTERVAL
-from homeassistant.helpers.aiohttp_client import async_get_clientsession
 import logging
-
 from .api import HWAMApi
 from .const import DOMAIN, DEFAULT_NAME, DEFAULT_SCAN_INTERVAL
 
@@ -18,7 +16,6 @@ DATA_SCHEMA = vol.Schema({
 
 class HWAMConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for HWAM."""
-
     VERSION = 1
     CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_POLL
 
@@ -27,28 +24,21 @@ class HWAMConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         errors = {}
 
         if user_input is not None:
-            _LOGGER.debug("Config flow received user input: %s", user_input)
-            
-            session = async_get_clientsession(self.hass)
+            session = self.hass.helpers.aiohttp_client.async_get_clientsession()
             api = HWAMApi(user_input[CONF_HOST], session)
 
             try:
                 if await api.async_validate_connection():
-                    _LOGGER.debug("Connection test successful")
                     await api.close()
                     return self.async_create_entry(
                         title=user_input[CONF_NAME],
                         data=user_input
                     )
-                
-                _LOGGER.error("Could not connect to HWAM stove")
                 errors["base"] = "cannot_connect"
-                
             except asyncio.TimeoutError:
-                _LOGGER.error("Timeout connecting to HWAM stove")
                 errors["base"] = "timeout"
-            except Exception as err:
-                _LOGGER.exception("Unexpected error occurred during connection test: %s", err)
+            except Exception:
+                _LOGGER.exception("Unexpected error")
                 errors["base"] = "unknown"
             finally:
                 await api.close()
