bb8015a5f079569409732c119f28b439d9ac6b96
Digital-Munebox <pinaud.adrien@free.fr>
2024-11-29 17:11:18 +0100

    Update services.py

--- services.py
+++ services.py
@@ -15,63 +15,39 @@ BASE_SCHEMA = vol.Schema({
     vol.Required(CONF_DEVICE_ID): str,
 })
 
-async def handle_set_night_mode_hours(hass: HomeAssistant, service_call: ServiceCall):
-    """Handle set_night_mode_hours service."""
-    device_id = service_call.data[CONF_DEVICE_ID]
-    coordinator = hass.data[DOMAIN][device_id]["coordinator"]
-    api = hass.data[DOMAIN][device_id]["api"]
+def register_services(hass: HomeAssistant) -> None:
+    """Register services."""
     
-    await api.set_night_mode_hours(
-        service_call.data[ATTR_START_TIME],
-        service_call.data[ATTR_END_TIME]
-    )
-    await coordinator.async_request_refresh()
-
-async def handle_enable_night_mode(hass: HomeAssistant, service_call: ServiceCall):
-    """Handle enable_night_mode service."""
-    device_id = service_call.data[CONF_DEVICE_ID]
-    coordinator = hass.data[DOMAIN][device_id]["coordinator"]
-    api = hass.data[DOMAIN][device_id]["api"]
-    
-    await api.set_night_mode(True)
-    await coordinator.async_request_refresh()
-
-async def handle_disable_night_mode(hass: HomeAssistant, service_call: ServiceCall):
-    """Handle disable_night_mode service."""
-    device_id = service_call.data[CONF_DEVICE_ID]
-    coordinator = hass.data[DOMAIN][device_id]["coordinator"]
-    api = hass.data[DOMAIN][device_id]["api"]
-    
-    await api.set_night_mode(False)
-    await coordinator.async_request_refresh()
-
-async def async_setup_services(hass: HomeAssistant) -> bool:
-    """Set up HWAM services."""
-    if not hass.data.get(DOMAIN):
-        hass.data[DOMAIN] = {}
-
-    if not hass.services.has_service(DOMAIN, "set_night_mode_hours"):
-        hass.services.async_register(
-            DOMAIN,
-            "set_night_mode_hours",
-            handle_set_night_mode_hours,
-            schema=TIME_SCHEMA,
+    async def set_night_mode_hours(call: ServiceCall) -> None:
+        """Handle set_night_mode_hours service."""
+        device_id = call.data[CONF_DEVICE_ID]
+        coordinator = hass.data[DOMAIN][device_id]["coordinator"]
+        api = hass.data[DOMAIN][device_id]["api"]
+        
+        await api.set_night_mode_hours(
+            call.data[ATTR_START_TIME],
+            call.data[ATTR_END_TIME]
         )
+        await coordinator.async_request_refresh()
 
-    if not hass.services.has_service(DOMAIN, "enable_night_mode"):
-        hass.services.async_register(
-            DOMAIN,
-            "enable_night_mode",
-            handle_enable_night_mode,
-            schema=BASE_SCHEMA,
-        )
+    async def enable_night_mode(call: ServiceCall) -> None:
+        """Handle enable_night_mode service."""
+        device_id = call.data[CONF_DEVICE_ID]
+        coordinator = hass.data[DOMAIN][device_id]["coordinator"]
+        api = hass.data[DOMAIN][device_id]["api"]
+        
+        await api.set_night_mode(True)
+        await coordinator.async_request_refresh()
 
-    if not hass.services.has_service(DOMAIN, "disable_night_mode"):
-        hass.services.async_register(
-            DOMAIN,
-            "disable_night_mode",
-            handle_disable_night_mode,
-            schema=BASE_SCHEMA,
-        )
+    async def disable_night_mode(call: ServiceCall) -> None:
+        """Handle disable_night_mode service."""
+        device_id = call.data[CONF_DEVICE_ID]
+        coordinator = hass.data[DOMAIN][device_id]["coordinator"]
+        api = hass.data[DOMAIN][device_id]["api"]
+        
+        await api.set_night_mode(False)
+        await coordinator.async_request_refresh()
 
-    return True
+    hass.services.async_register(DOMAIN, "set_night_mode_hours", set_night_mode_hours, schema=TIME_SCHEMA)
+    hass.services.async_register(DOMAIN, "enable_night_mode", enable_night_mode, schema=BASE_SCHEMA)
+    hass.services.async_register(DOMAIN, "disable_night_mode", disable_night_mode, schema=BASE_SCHEMA)
