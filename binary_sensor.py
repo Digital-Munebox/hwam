c7753d456872a159304eacd9a0e30103eb8ee859
Digital-Munebox <pinaud.adrien@free.fr>
2024-11-29 16:36:07 +0100

    Update binary_sensor.py

--- binary_sensor.py
+++ binary_sensor.py
@@ -1,7 +1,9 @@
 """Support for HWAM binary sensors."""
-from homeassistant.components.binary_sensor import BinarySensorEntity
+from homeassistant.components.binary_sensor import (
+    BinarySensorEntity,
+    BinarySensorDeviceClass,
+)
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
-
 from .const import DOMAIN
 
 async def async_setup_entry(hass, entry, async_add_entities):
@@ -9,24 +11,36 @@ async def async_setup_entry(hass, entry, async_add_entities):
     coordinator = hass.data[DOMAIN][entry.entry_id]["coordinator"]
 
     async_add_entities([
-        HWAMBinarySensor(coordinator, "door_open", "Porte ouverte"),
+        HWAMBinarySensor(
+            coordinator, 
+            "door_open", 
+            "Porte ouverte",
+            BinarySensorDeviceClass.DOOR
+        ),
+        HWAMBinarySensor(
+            coordinator,
+            "night_mode",
+            "Mode nuit",
+            BinarySensorDeviceClass.RUNNING
+        ),
     ])
-
 
 class HWAMBinarySensor(CoordinatorEntity, BinarySensorEntity):
     """Representation of a HWAM binary sensor."""
 
-    def __init__(self, coordinator, key, name):
+    def __init__(self, coordinator, key, name, device_class):
         super().__init__(coordinator)
         self._key = key
-        self._name = name
-        self._attr_is_on = self.coordinator.data.get(key)
-
-    @property
-    def name(self):
-        return self._name
+        self._attr_name = name
+        self._attr_unique_id = f"{coordinator.config_entry.entry_id}_{key}"
+        self._attr_device_class = device_class
+        self._attr_device_info = {
+            "identifiers": {(DOMAIN, coordinator.config_entry.entry_id)},
+            "name": coordinator.config_entry.data.get("name", "HWAM Stove"),
+            "manufacturer": "HWAM",
+        }
 
     @property
     def is_on(self):
-        """Return True if the binary sensor is on."""
-        return self._attr_is_on
+        """Return true if the binary sensor is on."""
+        return self.coordinator.data.get(self._key, False)
