5e91c4f87bf673df9c60404abf8f97dc81702617
Digital-Munebox <pinaud.adrien@free.fr>
2024-12-01 14:56:51 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -14,7 +14,7 @@ from .services import register_services
 
 _LOGGER = logging.getLogger(__name__)
 
-PLATFORMS = [Platform.SENSOR, Platform.BINARY_SENSOR, Platform.SELECT]
+PLATFORMS = [Platform.SENSOR, Platform.BINARY_SENSOR, Platform.SELECT, Platform.BUTTON]
 
 async def async_setup(hass: HomeAssistant, _) -> bool:
     """Set up HWAM integration."""
